c6b78ffc9d0a02771f7903c7040cdd84c7ba80a5
brasilico.ivan@gmail.com <Iv@n1234>
2019-05-31 17:27:57 -0300

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,15 @@
-
 import sys
+
 from apiserver.models import orm
 
 sys.path.insert(0, './apiserver')
 
 from apiserver.main import create_app
 
+session, engine = orm.init_db()
+orm.Base.metadata.drop_all(bind=engine)
+orm.Base.metadata.create_all(bind=engine)
+app = create_app(session, engine)
+
 if __name__ == '__main__':
-    session, engine = orm.init_db()
-    orm.Base.metadata.drop_all(bind=engine)
-    orm.Base.metadata.create_all(bind=engine)
-    app = create_app(session, engine)
     app.run(port=8000)
