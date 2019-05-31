c115a11a947e5aa9970e5c099640e40ce73797c7
brasilico.ivan@gmail.com <Iv@n1234>
2019-05-31 17:30:26 -0300

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,16 @@ sys.path.insert(0, './apiserver')
 
 from apiserver.main import create_app
 
-session, engine = orm.init_db()
-orm.Base.metadata.drop_all(bind=engine)
-orm.Base.metadata.create_all(bind=engine)
+session, engine = orm.init_db('sqlite:///:memory:')
+
+try:
+    orm.Base.metadata.drop_all(bind=engine)
+except:
+    pass
+try:
+    orm.Base.metadata.create_all(bind=engine)
+except:
+    pass
 app = create_app(session, engine)
 
 if __name__ == '__main__':
