fdc01aaf250666dba9106365ce9465e8d3ca2d79
brasilico.ivan@gmail.com <Iv@n1234>
2019-06-02 21:31:22 -0300

    Criando banco na inicialização wsgi (para heroku)

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,9 @@ sys.path.insert(0, './apiserver')
 from apiserver.main import create_app
 
 session, engine = orm.init_db()
+orm.Base.metadata.drop_all(bind=engine)
+orm.Base.metadata.create_all(bind=engine)
 app = create_app(session, engine)
 
 if __name__ == '__main__':
-    orm.Base.metadata.drop_all(bind=engine)
-    orm.Base.metadata.create_all(bind=engine)
     app.run(port=8000, threaded=False)
